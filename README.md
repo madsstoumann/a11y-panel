25913f75fe4339593d5fd2f010281f766d170a67
Mads Stoumann <stoumann@gmail.com>
2019-07-22 13:04:36 +0200

    Added CSS-based Zoom

--- README.md
+++ README.md
@@ -1,2 +1,168 @@
 # a11y-panel
-Panel with accessibility features
+Accessibility-panel with contrast/brightness, font-size/zoom, color-modes and highlight of links / focusable elements.
+
+![A11y Panel](/docs/a11y.png)
+
+## Features
+All the panel's features are configurable and can be shown/hidden via the options-object or data-attributes.
+
+### Contrast
+Updates a global CSS-variable, `--contrast`, and applies that to a CSS-filter, targetting all images and background-images.
+
+### Brightness
+Updates a global CSS-variable, `--brightness`, and applies that to a CSS-filter, targetting all images and background-images.
+
+### Apply to images
+Adds the class defined in `clsBodyAll` (default `a11y--all`) to `<body>`, targetting *all* elements with brightness/contrast instead of just images.
+
+### Color mode
+Updates two global CSS-variables, `--invert` and `--grayscale`, and applies these to a CSS-filter, changing the overall colors of the page. When both is set to `0` it equals `default`.
+
+### Font-size
+Updates a global CSS-variable, `--fontsize`, and applies that to `<body>`.  
+Font-sizes must be defined in relative units, like `em` or `rem`, for this to work.  
+Disabled by default.
+
+### Zoom
+Updates a global CSS-variable, `--zoom`, and applies that to `<body>`.  
+This method scales everything via CSS, and isonly recommended if font-size-scaling can't be used (see above).  
+Disabled by default.
+
+## Focusable (highlight tab-stops)
+Adds the class defined in `clsFocusable` (default `a11y--focus`) to all focusable elements, and a counter to a `::before`-pseudo-element, numbering all focusable elements in order.
+
+
+
+*Below: All settings enabled*
+
+![A11y Panel](/docs/a11y__all.png)
+
+## Install
+Copy `a11yPanel.mjs` and `a11y.css` to a location of your choice within your project, and integrate them in your webpack/parcel-setup.
+
+You need a small chunk of HTML around the button/link you'll use to trigger the panel:
+
+```html
+<!-- Begin A11y Panel -->
+<nav
+  class="a11y-panel__wrapper"
+  data-js="a11yPanel">
+  <button
+    aria-label="Toggle Accessibility Settings, use shift + ?"
+    class="a11y-panel__toggle"
+    role="button">
+    Text here ...
+  </button>
+</nav>
+<!-- End A11y Panel -->
+```
+
+To load the panel, use this (or copy it to your main JavaScript-file):
+
+```js
+import A11yPanel from './yourLocation/a11yPanel.mjs';
+const panel = document.querySelector('[data-js*="a11yPanel"]');
+if (panel) new A11yPanel(panel, panel.dataset);
+``` 
+
+If the CSS is not a part of your build, add it to `<head>`:
+
+```html
+<link href="/yourLocation/a11y.css" rel="stylesheet">
+```
+
+## Settings
+Settings can either be defined as an object:
+
+```js
+new A11yPanel(panel, { showContrast: false });
+```
+
+Or as data-attributes (note the data-prefix as well as k-e-b-a-b-case instead of camelCase):
+
+```html
+<nav
+  class="a11y-panel__wrapper"
+  data-js="a11yPanel"
+  data-show-contrast="false"
+  data-show-fontsize="true">
+```
+
+```js
+new A11yPanel(panel, panel.dataset);
+```
+
+## Options
+
+### CSS Classes
+
+- clsBody. Added to `<body>`. 
+- clsBodyAll. Added to `<body>` if "Apply to all content".
+- clsClose. Close-button.
+- clsDialog. Main class for panel.
+- clsFocusable. Added to focusable elements.
+- clsForm. Added to inner form of panel.
+
+### Labels
+
+| Setting              | Default                  |
+| :------------------- | :----------------------- |
+| labelApply           | 'Apply to images, or'    |
+| labelBrightness      | 'brightness'             |
+| labelClose           | '&#10005;'               |
+| labelColorMode       | 'Color mode'             |
+| labelContrast        | 'Contrast'               |
+| labelDefault         | 'Default'                |
+| labelFocusable       | 'tab-stops'              |
+| labelFocusableHeader | 'Highlight'              |
+| labelFontsize        | 'Fontsize'               |
+| labelGrayscale       | 'Grayscale'              |
+| labelAllContent      | 'All content'            |
+| labelInverted        | 'Inverted'               |
+| labelReset           | 'Reset'                  |
+| labelSave            | 'Save'                   |
+| labelZoom            | 'Zoom'                   |
+
+### Brightness
+
+| Setting          | Default    |
+| :--------------- | :--------- |
+| maxBrightness    | 200        |
+| minBrightness    | 100        |
+| valueBrightness  | 0          |
+
+### Contrast
+
+| Setting          | Default    |
+| :--------------- | :--------- |
+| maxContrast      | 200        |
+| minContrast      | 100        |
+| valueContrast    | 0          |
+
+### Font-size
+
+| Setting          | Default    |
+| :--------------- | :--------- |
+| maxFontsize      | 300        |
+| minFontsize      | 100        |
+| valueFontsize    | 0          |
+
+### Zoom
+
+| Setting          | Default    |
+| :--------------- | :--------- |
+| maxZoom          | 300        |
+| minZoom          | 100        |
+| valueZoom        | 0          |
+
+## Show / Hide features
+
+| Setting          | Default    |
+| :--------------- | :--------- |
+| showApplyImages  | true       |
+| showBrightness   | true       |
+| showColorMode    | true       |
+| showContrast     | true       |
+| showFocusable    | false      |
+| showFontsize     | false      |
+| showZoom         | false      |
--- js/a11yPanel.mjs
+++ js/a11yPanel.mjs
@@ -1,11 +1,11 @@
+/* eslint no-empty: ["error", { "allowEmptyCatch": true }] */
+
 /**
  * A11y Panel.
  * @module a11yPanel.mjs
- * @version 0.1.1
+ * @version 0.1.5
  * @author Mads Stoumann
  * @description Accessibility Settings-panel: Adjust brightness, contrast, color-modes and more.
- * TODO: TEST IN ALL BROWSERS
- * TODO: Mobile panbel with scroll
  */
 
 export default class A11yPanel {
@@ -16,6 +16,7 @@ export default class A11yPanel {
         clsBodyAll: 'a11y--all',
         clsClose: 'a11y-panel__close',
         clsDialog: 'a11y-panel',
+        clsFocusable: 'a11y--focus',
         clsForm: 'a11y-panel__form',
 
         labelApply: 'Apply to images, or',
@@ -24,60 +25,67 @@ export default class A11yPanel {
         labelColorMode: 'Color mode',
         labelContrast: 'Contrast',
         labelDefault: 'Default',
+        labelFocusable: 'Tab-stops',
+        labelFocusableHeader: 'Highlight',
+        labelFontsize: 'Fontsize',
         labelGrayscale: 'Grayscale',
         labelAllContent: 'All content',
         labelInverted: 'Inverted',
         labelReset: 'Reset',
         labelSave: 'Save',
-        labelZoomBrowser:
-          'Use <kbd>Ctrl+</kbd> to zoom in and <kbd>Ctrl-</kbd> to zoom out.',
-        labelZoomLevel: 'Current Zoom-level',
+        labelZoom: 'Zoom',
 
         maxBrightness: 200,
         minBrightness: 0,
-        optionsBrightness: 5,
         valueBrightness: 100,
 
         maxContrast: 200,
         minContrast: 0,
-        optionsContrast: 5,
-        valueContrast: 100
+        valueContrast: 100,
+
+        maxFontsize: 300,
+        minFontsize: 100,
+        valueFontsize: 100,
+
+        maxZoom: 300,
+        minZoom: 100,
+        valueZoom: 100,
+
+        showApplyImages: true,
+        showBrightness: true,
+        showColorMode: true,
+        showContrast: true,
+        showFocusable: false,
+        showFontsize: false,
+        showZoom: false
       },
-      settings
+      this.datasetToObject(settings)
     );
 
-    this.currentZoomLevel = 100;
     this.dialogSupported = typeof HTMLDialogElement === 'function';
     this.wrapper = wrapper;
     this.toggle = wrapper.firstElementChild;
+    this.isEdge = /Edge/.test(navigator.userAgent);
     this.initState();
     this.init();
+    console.log(this);
   }
 
   /**
-   * @function createOptions
-   * @description Adds optiosn to a <datalist>
-   * @param {Number} total
-   * @param {Number} min
-   * @param {Number} max
+   * @function datasetToObject
+   * @param {Object} dataset
+   * @description Convert a DOM node dataset to object with type convertions
    */
-  createOptions(total, min, max) {
-    return new Array(total + 1)
-      .fill(undefined)
-      .map(
-        (option, index) => `<option>${((max - min) / total) * index}</option>`
-      )
-      .join('');
-  }
-
-  /**
-   * @function getZoomLevel
-   * @description Returns current zoomLevel
-   */
-  getZoomLevel() {
-    if (this.zoomLevel) {
-      this.zoomLevel.innerText = Math.round(window.devicePixelRatio * 100);
-    }
+  datasetToObject(dataset) {
+    let obj = Object.assign({}, dataset);
+    Object.keys(obj).forEach(key => {
+      let val = obj[key];
+      try {
+        val = JSON.parse(val);
+      } catch (err) {}
+      obj[key] = val;
+    });
+    return obj;
   }
 
   /**
@@ -85,6 +93,16 @@ export default class A11yPanel {
    * @description Create DOM-nodes, add event-listeners
    */
   init() {
+    /* Add info to all focusable items */
+    this.focusable = document.querySelectorAll(
+      'button, a[href], select, textarea, input:not([type="hidden"])'
+    );
+    this.focusable.forEach((item, index) => {
+      item.dataset.focus = index;
+      item.classList.add(this.settings.clsFocusable);
+    });
+
+    /* Create panel elements */
     this.close = document.createElement('button');
     this.close.classList.add(this.settings.clsClose);
     this.close.innerHTML = this.settings.labelClose;
@@ -100,7 +118,6 @@ export default class A11yPanel {
       this.save();
     });
 
-    this.zoomLevel = this.form.querySelector('[data-zoom-level]');
     this.dialog = document.createElement('dialog');
     this.dialog.classList.add(this.settings.clsDialog);
     this.dialog.appendChild(this.close);
@@ -127,7 +144,7 @@ export default class A11yPanel {
         default:
       }
     });
-    window.addEventListener('resize', () => this.getZoomLevel());
+
     this.load();
   }
 
@@ -140,7 +157,10 @@ export default class A11yPanel {
       allContent: false,
       brightness: this.settings.valueBrightness,
       colorMode: 0,
-      contrast: this.settings.valueContrast
+      contrast: this.settings.valueContrast,
+      focusable: 0,
+      fontsize: this.settings.valueFontsize,
+      zoom: this.settings.valueZoom
     };
   }
 
@@ -153,9 +173,12 @@ export default class A11yPanel {
       const state = JSON.parse(window.localStorage.getItem('a11ypanel'));
       if (state) {
         this.state = state;
+        this.form['a11y-panel-allContent'].checked = this.state.allContent;
         this.form['a11y-panel-brightness'].value = this.state.brightness;
         this.form['a11y-panel-contrast'].value = this.state.contrast;
-        this.form['a11y-panel-allContent'].checked = this.state.allContent;
+        this.form['a11y-panel-focusable'].checked = this.state.focusable === 1;
+        this.form['a11y-panel-fontsize'].value = this.state.fontsize;
+        this.form['a11y-panel-zoom'].value = this.state.zoom;
         this.form.elements.colormode.value = this.state.colorMode;
         this.setProperties();
       }
@@ -200,6 +223,9 @@ export default class A11yPanel {
   setProperties() {
     this.setProperty('brightness', this.state.brightness, '%');
     this.setProperty('contrast', this.state.contrast, '%');
+    this.setProperty('fontsize', this.state.fontsize, '%');
+    this.setProperty('focusable', this.state.focusable);
+    this.setProperty('zoom', this.state.zoom, '%');
     this.setProperty('grayscale', this.state.colorMode === 2 ? 1 : 0);
     this.setProperty('invert', this.state.colorMode === 1 ? 1 : 0);
     document.body.classList.toggle(
@@ -230,6 +256,15 @@ export default class A11yPanel {
       case 'colorMode':
         this.state.colorMode = value;
         break;
+      case 'focusable':
+        this.state.focusable = target.checked ? 1 : 0;
+        break;
+      case 'fontsize':
+        this.state.fontsize = value;
+        break;
+      case 'zoom':
+        this.state.zoom = value;
+        break;
       default:
         break;
     }
@@ -242,6 +277,9 @@ export default class A11yPanel {
    */
   template() {
     return `
+    ${
+      this.settings.showContrast
+        ? `
     <div class="a11y-panel-field">
       <label class="a11y-panel-field__label o-icon__label o-icon--contrast" for="a11y-panel-contrast">
         <strong class="a11y-panel-field__label-text">${
@@ -250,24 +288,20 @@ export default class A11yPanel {
       </label>
       <input
         autofocus
-        class="a11y-panel-field__range"
+        class="a11y-panel-field__range${this.isEdge ? '--edge' : ''}"
         data-state="contrast"
         id="a11y-panel-contrast"
-        list="a11y-panel-contrast-list"
         min="${this.settings.minContrast}"
         max="${this.settings.maxContrast}"
         type="range"
         value="${this.settings.valueContrast}"
         />
-      <datalist id="a11y-panel-contrast-list" class="a11y-panel-field__range-ticks">
-        ${this.createOptions(
-          this.settings.optionsContrast,
-          this.settings.minContrast,
-          this.settings.maxContrast
-        )}
-      </datalist>
-    </div>
-
+    </div>`
+        : ''
+    }
+    ${
+      this.settings.showBrightness
+        ? `
     <div class="a11y-panel-field">
       <label class="a11y-panel-field__label o-icon__label o-icon--brightness" for="a11y-panel-brightness">
         <strong class="a11y-panel-field__label-text">${
@@ -275,44 +309,85 @@ export default class A11yPanel {
         }</strong>
       </label>
       <input
-        class="a11y-panel-field__range"
+        class="a11y-panel-field__range${this.isEdge ? '--edge' : ''}"
         data-state="brightness"
         id="a11y-panel-brightness"
-        list="a11y-panel-brightness-list"
         min="${this.settings.minBrightness}"
         max="${this.settings.maxBrightness}"
         type="range"
         value="${this.settings.valueBrightness}"
       />
-      <datalist class="a11y-panel-field__range-ticks" id="a11y-panel-brightness-list">
-      ${this.createOptions(
-        this.settings.optionsBrightness,
-        this.settings.minBrightness,
-        this.settings.maxBrightness
-      )}
-      </datalist>
-    </div>
-
+    </div>`
+        : ''
+    }
+    ${
+      this.settings.showApplyImages
+        ? `
     <div class="a11y-panel-field--noborder">
-    <fieldset class="a11y-panel-field__group">
-      <legend class="a11y-panel-field__legend a11y-panel-field__label">
+      <fieldset class="a11y-panel-field__group">
+        <legend class="a11y-panel-field__legend a11y-panel-field__label">
+          <strong class="a11y-panel-field__label-text">${
+            this.settings.labelApply
+          }</strong>
+        </legend>
+        <label class="a11y-panel-field__checkbox a11y-panel-field__checkbox--switch">
+          <input type="checkbox" id="a11y-panel-allContent" class="u-visually-hidden" data-state="allContent" />
+          <span class="a11y-panel-field__group-item a11y-panel-field__checkbox-text">${
+            this.settings.labelAllContent
+          }</span>
+        </label>
+      </fieldset>
+    </div>`
+        : ''
+    }
+    ${
+      this.settings.showFontsize
+        ? `
+    <div class="a11y-panel-field">
+      <label class="a11y-panel-field__label o-icon__label o-icon--fontsize" for="a11y-panel-fontsize">
         <strong class="a11y-panel-field__label-text">${
-          this.settings.labelApply
+          this.settings.labelFontsize
         }</strong>
-      </legend>
-
-      <label class="a11y-panel-field__checkbox a11y-panel-field__checkbox--switch">
-        <input type="checkbox" id="a11y-panel-allContent" class="u-visually-hidden" data-state="allContent" />
-        <span class="a11y-panel-field__group-item a11y-panel-field__checkbox-text">${
-          this.settings.labelAllContent
-        }</span>
       </label>
-    </fieldset>
-  </div>
-
+      <input
+        class="a11y-panel-field__range${this.isEdge ? '--edge' : ''}"
+        data-state="fontsize"
+        id="a11y-panel-fontsize"
+        min="${this.settings.minFontsize}"
+        max="${this.settings.maxFontsize}"
+        type="range"
+        value="${this.settings.valueFontsize}"
+      />
+    </div>`
+        : ''
+    }
+    ${
+      this.settings.showZoom
+        ? `
+    <div class="a11y-panel-field">
+      <label class="a11y-panel-field__label o-icon__label o-icon--zoom" for="a11y-panel-zoom">
+        <strong class="a11y-panel-field__label-text">${
+          this.settings.labelZoom
+        }</strong>
+      </label>
+      <input
+        class="a11y-panel-field__range${this.isEdge ? '--edge' : ''}"
+        data-state="zoom"
+        id="a11y-panel-zoom"
+        min="${this.settings.minZoom}"
+        max="${this.settings.maxZoom}"
+        type="range"
+        value="${this.settings.valueZoom}"
+      />
+    </div>`
+        : ''
+    }
+    ${
+      this.settings.showColorMode
+        ? `
     <div class="a11y-panel-field  a11y-panel-field--noborder">
       <fieldset class="a11y-panel-field__group">
-        <legend class="a11y-panel-field__legend a11y-panel-field__label o-icon__label o-icon--color">
+        <legend class="a11y-panel-field__legend a11y-panel-field__label">
             <strong class="a11y-panel-field__label-text">${
               this.settings.labelColorMode
             }</strong>
@@ -336,18 +411,29 @@ export default class A11yPanel {
             }</span>
         </label>
       </fieldset>
-    </div>
-
-    <div class="a11y-panel-field a11y-panel-field--noborder a11y-panel-field__label">
-      <strong class="a11y-panel-field__label-text">${
-        this.settings.labelZoomLevel
-      }:
-      </strong>
-      <span data-zoom-level>${this.currentZoomLevel}</span>%
-    </div>
-
-    <p hidden>${this.settings.labelZoomBrowser}</p>
-
+    </div>`
+        : ''
+    }
+    ${
+      this.settings.showFocusable
+        ? `
+    <div class="a11y-panel-field--noborder">
+      <fieldset class="a11y-panel-field__group">
+        <legend class="a11y-panel-field__legend a11y-panel-field__label">
+          <strong class="a11y-panel-field__label-text">${
+            this.settings.labelFocusableHeader
+          }</strong>
+        </legend>
+        <label class="a11y-panel-field__checkbox a11y-panel-field__checkbox--switch">
+          <input type="checkbox" id="a11y-panel-focusable" class="u-visually-hidden" data-state="focusable" />
+          <span class="a11y-panel-field__group-item a11y-panel-field__checkbox-text">${
+            this.settings.labelFocusable
+          }</span>
+        </label>
+      </fieldset>
+    </div>`
+        : ''
+    }
     <nav class="a11y-panel__buttons">
       <button type="reset" class="a11y-panel__reset">${
         this.settings.labelReset
